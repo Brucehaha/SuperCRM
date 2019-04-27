c2973c6cbbf4093065f8daab2c621cd509868795
brucehaha <henninglee2013@gmail.com>
2019-04-27 16:09:14 +1000

    paginator

--- SuperAdmin/utils/paginator.py
+++ SuperAdmin/utils/paginator.py
@@ -2,18 +2,20 @@ from django.utils.safestring import mark_safe
 
 
 class MyPaginator(object):
-    def __init__(self, total_items, url="", num_per_page=20, max_num_pages=7, current_page = 1):
+    def __init__(self, total_items, url="", num_per_page=20, max_num_pages=7, current_page=1):
         self.total_items = total_items
-        self.current_page = current_page
+        self.num_per_page = num_per_page
+        self.current_page = current_page if current_page <= self.total_pages() else self.total_pages()
         self.url = url
-        self.num_per_page = 20
         self.max_num_pages = max_num_pages
 
+    @property
     def start(self):
-        return (self.current_page-1)*self.total_items
+        return (self.current_page-1)*self.num_per_page
 
+    @property
     def end(self):
-        return self.current_page*self.total_items
+        return self.current_page*self.num_per_page
 
     def total_pages(self):
         a, b = divmod(self.total_items, self.num_per_page)
@@ -25,7 +27,7 @@ class MyPaginator(object):
         first = last = 1
 
         if self.total_pages() < self.max_num_pages:
-            return list(range(first, self.total_pages()))
+            return list(range(first, self.total_pages()+1))
 
         elif self.current_page <= self.max_num_pages//2:
             last = self.max_num_pages
@@ -37,36 +39,33 @@ class MyPaginator(object):
                 first = 1
             if last > self.total_pages():
                 last = self.total_pages()
-        return list(range(first, last))
+        return list(range(first, last+1))
 
     def paginator(self):
-        _html = "<ul class='pagination'><li class='page-item'><a class='page-link' href ='%s?p=%s'>First Page</a></li>" %(
-                    self.url,
-                    1)
+        _html = "<ul class='pagination'><li class='page-item'>"
         is_active = ''
-        if self.current_page != 1:
-            _html += "<li class='page-item'><a class='page-link' href ='%s?p=%s'>Prev</a></li>" %(
+        if self.current_page > 1 and self.total_pages() > 4:
+            _html += "<a class='page-link' href ='%s?p=%s'>First Page</a></li>" % (self.url, 1)
+            _html += "<li class='page-item'><a class='page-link' href ='%s?p=%s'>Prev</a></li>" % (
                     self.url,
                     self.current_page - 1)
-        else:
-            _html += "<ul class='pagination'>" \
-                     "<li class='page-item'><a class='page-link' href ='javascript:void(0)'>Previous</a></li>"
 
         for i in self.page_range():
             if self.current_page == i:
-                is_active='active'
+                is_active = 'active'
+            else:
+                is_active = ''
 
             _html += "<li class='page-item  %s'><a class='page-link' href='%s?p=%s'>%s</a></li>" % (
                     is_active,
                     self.url,
                     i, i)
-
-        _html += "<li class='page-item'>" \
-                 "<a class='page-link' href ='%s?p=%s'>Next</a></li></ul>" % (self.url, self.current_page + 1)
-
-        _html += "<li class='page-item'><a class='page-link' href ='%s?p=%s'>Last Page</a></li></ul>" % (
+        if self.current_page < self.total_pages() and self.total_pages() > 4:
+            _html += "<li class='page-item'>" \
+                     "<a class='page-link' href ='%s?p=%s'>Next</a></li>" % (self.url, self.current_page + 1)
+            _html += "<li class='page-item'><a class='page-link' href ='%s?p=%s'>Last Page</a></li>" % (
             self.url, self.total_pages())
-
+        _html += "</ul>"
         _html += "%s Page of %s" % (
             self.current_page,  self.total_pages()
         )
--- SuperAdmin/views.py
+++ SuperAdmin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 
+from .utils.paginator import  MyPaginator
 from SuperAdmin import app_setup
 
 app_setup.superadmin_auto_discover()
@@ -14,25 +15,32 @@ from SuperAdmin.sites import site
 def app_index(request):
     return render(request, 'superadmin/app_index.html', {'site': site})
 
+
 def table_filter(request, queryset):
     filter_conditions = {}
     for k, v in request.GET.items():
-        if v:
+        if v and k != 'p':
             filter_conditions[k] = v
     return queryset.filter(**filter_conditions), filter_conditions
 
-def table_list(request, app_name, model_name):
 
+def table_list(request, app_name, model_name):
     admin_class = site.enabled_admins[app_name][model_name]
+    p = request.GET.get('p', 1)
+    total = admin_class.model.objects.count()
+    page = MyPaginator(current_page=int(p), total_items=total, num_per_page=1)
     queryset = admin_class.model.objects.all()
     res, filter_conditions = table_filter(request, queryset)
+    print(page.start, page.end)
+    res = res[page.start:page.end]
     admin_class.filter_conditions = filter_conditions
 
-    return render(request, 'superadmin/table_list.html', {'queryset': res, 'admin_class': admin_class})
+    return render(request, 'superadmin/table_list.html', {'queryset': res, 'admin_class': admin_class, 'page': page})
 
 
 def app_list(request):
     return HttpResponse("app_list")
+
 
 def acc_signin(request):
     error_msg = ''
